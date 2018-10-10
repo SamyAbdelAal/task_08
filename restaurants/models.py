6f6dd80bc7e5dcbbd1b834f978040d4e22567eb1
Samy AbdelAal <sam.omran@hotmail.com>
2018-10-10 11:00:38 +0300

    task submission

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=120)
-    description = models.TextField()
-    opening_time = models.TimeField()
-    closing_time = models.TimeField()
+	name = models.CharField(max_length=120)
+	description = models.TextField()
+	logo= models.ImageField(blank=True, null=True)
+	opening_time = models.TimeField()
+	closing_time = models.TimeField()
